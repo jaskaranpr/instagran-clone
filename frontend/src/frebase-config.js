988442782b1b9faca0e5df56856f8ee93fcfafe1
Jakaran <80308625+jaskaranproductions@users.noreply.github.com>
2022-03-18 18:48:32 +0530

    remove firebase config

--- frontend/src/frebase-config.js
+++ frontend/src/frebase-config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDNUnK3rpMQV0IufJ1vi3aCIQrs3InKO5w",
-  authDomain: "instagram-6f62e.firebaseapp.com",
-  projectId: "instagram-6f62e",
-  storageBucket: "instagram-6f62e.appspot.com",
-  messagingSenderId: "847612019323",
-  appId: "1:847612019323:web:a213c9066ca680a684d8de",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 export const app = initializeApp(firebaseConfig);
